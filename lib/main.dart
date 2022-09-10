f698d1ad1818d981f8a094a28e54996a94534dfe
ldt <dthuanle.20@gmail.com>
2022-09-10 11:15:09 +0700

    fix app life cycle listener

--- lib/main.dart
+++ lib/main.dart
@@ -68,6 +68,18 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   }
 
   @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     super.didChangeAppLifecycleState(state);
     switch (state) {
